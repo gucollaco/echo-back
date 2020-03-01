a339962eadf261bbda9465304a5403a47e27467e
Gustavo Collaço <gustavo.m.collaco@gmail.com>
2020-03-01 14:14:34 -0300

    index files created

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-// const routes = require('./routes')
+const routes = require('./routes')
  
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
  
 app.get('/', (req, res) => res.send('App is working'))
  
-// app.use('/api', routes)
+app.use('/api', routes)
  
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
  
